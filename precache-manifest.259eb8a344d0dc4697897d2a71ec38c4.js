9d1a7de32613641a3277d82caed8b0b0e067c28a
AlsonZ <48876218+AlsonZ@users.noreply.github.com>
2020-03-03 16:02:16 +1100

    Updates

---
+++ precache-manifest.259eb8a344d0dc4697897d2a71ec38c4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e4458b8d4b7abe1696097478f101422",
+    "url": "/exchange-rate-calculator/index.html"
+  },
+  {
+    "revision": "1b38cba82119fff3922a",
+    "url": "/exchange-rate-calculator/static/css/main.35d636fd.chunk.css"
+  },
+  {
+    "revision": "80eec78f46338ac6c94c",
+    "url": "/exchange-rate-calculator/static/js/2.52480a49.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/exchange-rate-calculator/static/js/2.52480a49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b38cba82119fff3922a",
+    "url": "/exchange-rate-calculator/static/js/main.d0235e67.chunk.js"
+  },
+  {
+    "revision": "ed60d527ab9035209010",
+    "url": "/exchange-rate-calculator/static/js/runtime-main.5f6ef74d.js"
+  },
+  {
+    "revision": "0188afaf67ad5f690d9f86e473beb31d",
+    "url": "/exchange-rate-calculator/static/media/exchange.0188afaf.svg"
+  }
+]);
